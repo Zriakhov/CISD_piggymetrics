9106c1ead0aafe767e1b8ab8c094ef9ee78f8a92
Mykhailo_Zriakhov@epam.com <Zriakhov_Mykhailo@epam.com>
2020-01-10 18:03:09 +0200

    Added tag :stable

--- VM3_start.sh
+++ VM3_start.sh
@@ -10,25 +10,25 @@ if [ -n "${a}" ]; then
   echo "piggymetrics do'nt run"
 fi
 
-#docker pull 192.168.2.112:443/piggymetrics-config:latest
-docker pull 10.23.27.6:443/piggymetrics-config:latest
-#docker pull 192.168.2.112:443/piggymetrics-registry:latest
-docker pull 10.23.27.6:443/piggymetrics-registry:latest
-#docker pull 192.168.2.112:443/piggymetrics-gateway:latest
-docker pull 10.23.27.6:443/piggymetrics-gateway:latest
-#docker pull 192.168.2.112:443/piggymetrics-auth-service:latest
-docker pull 10.23.27.6:443/piggymetrics-auth-service:latest
-#docker pull 192.168.2.112:443/piggymetrics-mongodb:latest
-docker pull 10.23.27.6:443/piggymetrics-mongodb:latest
-#docker pull 192.168.2.112:443/piggymetrics-account-service:latest
-docker pull 10.23.27.6:443/piggymetrics-account-service:latest
-#docker pull 192.168.2.112:443/piggymetrics-statistics-service:latest
-docker pull 10.23.27.6:443/piggymetrics-statistics-service:latest
-#docker pull 192.168.2.112:443/piggymetrics-notification-service:latest
-docker pull 10.23.27.6:443/piggymetrics-notification-service:latest
-#docker pull 192.168.2.112:443/piggymetrics-monitoring:latest
-docker pull 10.23.27.6:443/piggymetrics-monitoring:latest
-#docker pull 192.168.2.112:443/piggymetrics-turbine-stream-service:latest
-docker pull 10.23.27.6:443/piggymetrics-turbine-stream-service:latest
+#docker pull 192.168.2.112:443/piggymetrics-config:stable
+docker pull 10.23.27.6:443/piggymetrics-config:stable
+#docker pull 192.168.2.112:443/piggymetrics-registry:stable
+docker pull 10.23.27.6:443/piggymetrics-registry:stable
+#docker pull 192.168.2.112:443/piggymetrics-gateway:stable
+docker pull 10.23.27.6:443/piggymetrics-gateway:stable
+#docker pull 192.168.2.112:443/piggymetrics-auth-service:stable
+docker pull 10.23.27.6:443/piggymetrics-auth-service:stable
+#docker pull 192.168.2.112:443/piggymetrics-mongodb:stable
+docker pull 10.23.27.6:443/piggymetrics-mongodb:stable
+#docker pull 192.168.2.112:443/piggymetrics-account-service:stable
+docker pull 10.23.27.6:443/piggymetrics-account-service:stable
+#docker pull 192.168.2.112:443/piggymetrics-statistics-service:stable
+docker pull 10.23.27.6:443/piggymetrics-statistics-service:stable
+#docker pull 192.168.2.112:443/piggymetrics-notification-service:stable
+docker pull 10.23.27.6:443/piggymetrics-notification-service:stable
+#docker pull 192.168.2.112:443/piggymetrics-monitoring:stable
+docker pull 10.23.27.6:443/piggymetrics-monitoring:stable
+#docker pull 192.168.2.112:443/piggymetrics-turbine-stream-service:stable
+docker pull 10.23.27.6:443/piggymetrics-turbine-stream-service:stable
 
 docker-compose up -d

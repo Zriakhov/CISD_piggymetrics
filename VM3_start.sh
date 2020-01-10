4e6c160731951c165ceb60b043400aa8f8ed8983
Mykhailo_Zriakhov@epam.com <Zriakhov_Mykhailo@epam.com>
2020-01-10 15:43:17 +0200

    Added TLS connection

--- VM3_start.sh
+++ VM3_start.sh
@@ -10,15 +10,25 @@ if [ -n "${a}" ]; then
   echo "piggymetrics do'nt run"
 fi
 
-docker pull 192.168.2.112:5000/piggymetrics-config:latest
-docker pull 192.168.2.112:5000/piggymetrics-registry:latest
-docker pull 192.168.2.112:5000/piggymetrics-gateway:latest
-docker pull 192.168.2.112:5000/piggymetrics-auth-service:latest
-docker pull 192.168.2.112:5000/piggymetrics-mongodb:latest
-docker pull 192.168.2.112:5000/piggymetrics-account-service:latest
-docker pull 192.168.2.112:5000/piggymetrics-statistics-service:latest
-docker pull 192.168.2.112:5000/piggymetrics-notification-service:latest
-docker pull 192.168.2.112:5000/piggymetrics-monitoring:latest
-docker pull 192.168.2.112:5000/piggymetrics-turbine-stream-service:latest
+#docker pull 192.168.2.112:443/piggymetrics-config:latest
+docker pull 10.23.27.6:443/piggymetrics-config:latest
+#docker pull 192.168.2.112:443/piggymetrics-registry:latest
+docker pull 10.23.27.6:443/piggymetrics-registry:latest
+#docker pull 192.168.2.112:443/piggymetrics-gateway:latest
+docker pull 10.23.27.6:443/piggymetrics-gateway:latest
+#docker pull 192.168.2.112:443/piggymetrics-auth-service:latest
+docker pull 10.23.27.6:443/piggymetrics-auth-service:latest
+#docker pull 192.168.2.112:443/piggymetrics-mongodb:latest
+docker pull 10.23.27.6:443/piggymetrics-mongodb:latest
+#docker pull 192.168.2.112:443/piggymetrics-account-service:latest
+docker pull 10.23.27.6:443/piggymetrics-account-service:latest
+#docker pull 192.168.2.112:443/piggymetrics-statistics-service:latest
+docker pull 10.23.27.6:443/piggymetrics-statistics-service:latest
+#docker pull 192.168.2.112:443/piggymetrics-notification-service:latest
+docker pull 10.23.27.6:443/piggymetrics-notification-service:latest
+#docker pull 192.168.2.112:443/piggymetrics-monitoring:latest
+docker pull 10.23.27.6:443/piggymetrics-monitoring:latest
+#docker pull 192.168.2.112:443/piggymetrics-turbine-stream-service:latest
+docker pull 10.23.27.6:443/piggymetrics-turbine-stream-service:latest
 
 docker-compose up -d

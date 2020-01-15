a9180b36f889ef5bb7e57a796fdda62fa3a62cd5
Mykhailo_Zriakhov@epam.com <Zriakhov_Mykhailo@epam.com>
2020-01-15 15:30:29 +0200

    Correction code

--- VM3_start.sh
+++ VM3_start.sh
@@ -10,25 +10,15 @@ if [ -n "${a}" ]; then
   echo "piggymetrics do'nt run"
 fi
 
-#docker pull 192.168.2.112:443/piggymetrics-config:stable
 docker pull 10.23.27.6:443/piggymetrics-config:stable
-#docker pull 192.168.2.112:443/piggymetrics-registry:stable
 docker pull 10.23.27.6:443/piggymetrics-registry:stable
-#docker pull 192.168.2.112:443/piggymetrics-gateway:stable
 docker pull 10.23.27.6:443/piggymetrics-gateway:stable
-#docker pull 192.168.2.112:443/piggymetrics-auth-service:stable
 docker pull 10.23.27.6:443/piggymetrics-auth-service:stable
-#docker pull 192.168.2.112:443/piggymetrics-mongodb:stable
 docker pull 10.23.27.6:443/piggymetrics-mongodb:stable
-#docker pull 192.168.2.112:443/piggymetrics-account-service:stable
 docker pull 10.23.27.6:443/piggymetrics-account-service:stable
-#docker pull 192.168.2.112:443/piggymetrics-statistics-service:stable
 docker pull 10.23.27.6:443/piggymetrics-statistics-service:stable
-#docker pull 192.168.2.112:443/piggymetrics-notification-service:stable
 docker pull 10.23.27.6:443/piggymetrics-notification-service:stable
-#docker pull 192.168.2.112:443/piggymetrics-monitoring:stable
 docker pull 10.23.27.6:443/piggymetrics-monitoring:stable
-#docker pull 192.168.2.112:443/piggymetrics-turbine-stream-service:stable
 docker pull 10.23.27.6:443/piggymetrics-turbine-stream-service:stable
 
 docker-compose up -d
